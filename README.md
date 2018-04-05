955723da1c40e0357fdf959167f8badb3f9b089e
ZUBOGU <gu.zubo@berkeley.edu>
2018-04-05 15:15:24 -0700

    start code the game

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # LearnReact
+
+## Playground
+Using website to practice for bigenning
+> https://jscomplete.com/repl/
+
+You can
+```
+    Render content here using the console.log() function
+    Use React/ReactDOM with the mountNode variable
+    Use this as a simple REPL
+```
---
+++ playground/GameApp.js
@@ -0,0 +1,48 @@
+const Stars = (props) => {
+  return (
+    <div>
+      <i className="fa fa-star"></i>
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button>=</button>
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return (
+    <div>
+    ...
+    </div>
+  );
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3> Play Nine</h3>
+        <Stars />
+        <Button />
+        <Answer />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, mountNode );
